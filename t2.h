1e75e44c0d8bdfc23ebf434aaefa70299afb35f5
Eligijus Skersonas <62078721+eli-scorpio@users.noreply.github.com>
2022-03-23 16:08:53 +0000

    Add files via upload

---
+++ t2.h
@@ -0,0 +1,12 @@
+
+// A procedure to calculate gcd recursively
+extern "C" _int64 gcd_recursion(_int64, _int64);
+
+// A procedure to get user input and add it to the max element of the array and print the result to the console
+extern "C" _int64 use_scanf(_int64, _int64 array[]);
+
+// A procedure to find the min of 4 parameters
+extern "C" _int64 min5(_int64, _int64, _int64, _int64);
+
+// A global variable 
+extern "C" _int64 inp_int;
